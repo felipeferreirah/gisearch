31d7806dbb8720b643e129153e2c945b355118a4
Felipe Ferreira Cabo <felipeferreirah@gmail.com>
2019-04-09 18:55:18 -0300

    Updates

---
+++ precache-manifest.a865dc6ccef13383604ea5e6f0aa3f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb62c818b24f9c023e11",
+    "url": "/felipeferreirah/gisearch/static/js/runtime~main.fb62c818.js"
+  },
+  {
+    "revision": "44e8800804d55e9eb0eb",
+    "url": "/felipeferreirah/gisearch/static/js/main.44e88008.chunk.js"
+  },
+  {
+    "revision": "72a006a426fed3d11fca",
+    "url": "/felipeferreirah/gisearch/static/js/1.72a006a4.chunk.js"
+  },
+  {
+    "revision": "44e8800804d55e9eb0eb",
+    "url": "/felipeferreirah/gisearch/static/css/main.ae299c1b.chunk.css"
+  },
+  {
+    "revision": "40dbf22544f26601e6eccd53d1526210",
+    "url": "/felipeferreirah/gisearch/index.html"
+  }
+];
