d44df5a8b423458b4b4ebe9db1e5233e4be37f5b
Felipe Ferreira Cabo <felipeferreirah@gmail.com>
2019-04-09 18:39:57 -0300

    Upload 'docs folder'.

---
+++ docs/precache-manifest.c6dc835390e05e2e4e07a49f7a23ed57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44e8800804d55e9eb0eb",
+    "url": "/static/js/main.44e88008.chunk.js"
+  },
+  {
+    "revision": "72a006a426fed3d11fca",
+    "url": "/static/js/1.72a006a4.chunk.js"
+  },
+  {
+    "revision": "44e8800804d55e9eb0eb",
+    "url": "/static/css/main.ae299c1b.chunk.css"
+  },
+  {
+    "revision": "90d119340732b6c40db565a2d19a4513",
+    "url": "/index.html"
+  }
+];
